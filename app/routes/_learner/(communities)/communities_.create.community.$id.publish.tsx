aa21234c895c0f77cff5c684f383369e6eeeb257
Dan Sivewright <dan@wixels.com>
2025-04-23 11:33:20 +0200

    start publish

--- app/routes/_learner/(communities)/communities_.create.community.$id.publish.tsx
+++ app/routes/_learner/(communities)/communities_.create.community.$id.publish.tsx
@@ -1,4 +1,6 @@
 import { useTRPC } from "@/integrations/trpc/react"
+import { cn } from "@/utils/cn"
+import { RiImageAddLine, RiImageLine } from "@remixicon/react"
 import {
   useMutation,
   useQueryClient,
@@ -7,6 +9,8 @@ import {
 import { createFileRoute, useNavigate } from "@tanstack/react-router"
 
 import { useNotification } from "@/hooks/use-notification"
+import { DotPattern } from "@/components/dot-pattern"
+import { Grid } from "@/components/grid"
 
 export const Route = createFileRoute(
   "/_learner/(communities)/communities_/create/community/$id/publish"
@@ -40,8 +44,44 @@ function RouteComponent() {
   )
   const updateCommunity = useMutation(trpc.communities.update.mutationOptions())
   return (
-    <div>
-      Hello "/_learner/(communities)/communities_/create/community/$id/publish"!
-    </div>
+    <Grid gap="none" className="h-[calc(100dvh-93px)] w-screen bg-bg-white-0">
+      <div className="col-span-6 h-full w-full p-2">
+        <div className="relative flex h-full w-full flex-col items-center justify-center overflow-hidden rounded-20 bg-bg-weak-50 px-6">
+          <DotPattern
+            className={cn(
+              "[mask-image:radial-gradient(400px_circle_at_center,white,transparent)]",
+              "translate-x-[5%] translate-y-[15%]",
+              "opacity-25"
+            )}
+          />
+          <div className="mx-auto flex h-full w-full max-w-[500px] flex-col items-start justify-center gap-4">
+            <header className="flex flex-col gap-1">
+              <h2 className="text-title-h6 font-normal">Preview</h2>
+              <p className="text-body-sm font-light text-text-soft-400">
+                This is how your community will appear.
+              </p>
+            </header>
+
+            <div className="relative z-10 h-[65%] w-full">
+              <div className="relative z-10 h-full w-full rounded-20 border-stroke-sub-300 bg-bg-white-0 p-2 drop-shadow-2xl"></div>
+              <div className="absolute -bottom-4 -right-4 h-full w-full rounded-20 bg-bg-weak-50"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-span-6 flex h-full w-full flex-col items-center justify-center bg-bg-white-0 p-2">
+        <div className="mx-auto flex h-full w-full max-w-[500px] flex-col items-start justify-center gap-4">
+          <RiImageAddLine className="size-10 fill-primary-base opacity-70" />
+          <header className="flex flex-col gap-1.5">
+            <h2 className="text-title-h5 font-normal">
+              Add your community images
+            </h2>
+            <p className="text-body-xs font-light text-text-soft-400">
+              Showcase your community with quality visuals
+            </p>
+          </header>
+        </div>
+      </div>
+    </Grid>
   )
 }
