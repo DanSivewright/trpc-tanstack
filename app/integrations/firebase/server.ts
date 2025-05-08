1ca2a2b4106b5f2fe7de102317c9e5e824bc8406
Dan Sivewright <dan@wixels.com>
2025-05-08 10:54:43 +0200

    firebase creds

--- app/integrations/firebase/server.ts
+++ app/integrations/firebase/server.ts
@@ -2,12 +2,27 @@ import { cert, getApps, initializeApp } from "firebase-admin/app"
 import { getAuth } from "firebase-admin/auth"
 import { getFirestore } from "firebase-admin/firestore"
 
-const serviceAccount = JSON.parse(import.meta.env.FIREBASE_SERVICE_ACCOUNT_KEY)
+function formatPrivateKey(key: string) {
+  return key.replace(/\\n/g, "\n")
+}
+
+const params = {
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID as string,
+  clientEmail: import.meta.env.FIREBASE_CLIENT_EMAIL as string,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET as string,
+  privateKey: import.meta.env.FIREBASE_PRIVATE_KEY as string,
+}
+
 // Initialize Firebase Admin only if no apps exist
 const app =
   getApps().length === 0
     ? initializeApp({
-        credential: cert(serviceAccount),
+        credential: cert({
+          projectId: params.projectId,
+          clientEmail: params.clientEmail,
+          privateKey: formatPrivateKey(params.privateKey),
+        }),
+        storageBucket: params.storageBucket,
       })
     : getApps()[0]
 
--- vite-end.d.ts
+++ vite-end.d.ts
@@ -7,7 +7,10 @@ interface ImportMetaEnv {
   readonly VITE_IMAGEKIT_PUBLIC_KEY: string
   readonly VITE_IMAGEKIT_URL_ENDPOINT: string
   readonly IMAGEKIT_KEY: string
-  readonly FIREBASE_SERVICE_ACCOUNT_KEY: string
+  readonly VITE_FIREBASE_PROJECT_ID: string
+  readonly VITE_FIREBASE_STORAGE_BUCKET: string
+  readonly FIREBASE_PRIVATE_KEY: string
+  readonly FIREBASE_CLIENT_EMAIL: string
 }
 
 interface ImportMeta {
