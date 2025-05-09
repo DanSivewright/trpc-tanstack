9ecf6086ef2ae22156cd44514af9a8b3c2234695
Dan Sivewright <dan@wixels.com>
2025-05-09 09:08:00 +0200

    loading state for root communities page

--- app/routes/_learner/communities/index.tsx
+++ app/routes/_learner/communities/index.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react"
+import { Suspense, useEffect, useState } from "react"
 import { useTRPC } from "@/integrations/trpc/react"
+import type { communitiesAllSchema } from "@/integrations/trpc/routers/communities/schemas/communities-schema"
 import { cn } from "@/utils/cn"
 import {
   RiArrowRightLine,
@@ -28,9 +29,10 @@ import {
   RiVideoLine,
   type RemixiconComponentType,
 } from "@remixicon/react"
-import { useQuery, useQueryClient } from "@tanstack/react-query"
+import { useQueryClient, useSuspenseQuery } from "@tanstack/react-query"
 import { createFileRoute, Link } from "@tanstack/react-router"
 import { motion } from "motion/react"
+import type { z } from "zod"
 
 import { useElementSize } from "@/hooks/use-element-size"
 import { Avatar } from "@/components/ui/avatar"
@@ -98,45 +100,20 @@ const filters: {
 export const Route = createFileRoute("/_learner/communities/")({
   component: RouteComponent,
   loader: async ({ context }) => {
-    await Promise.all([
-      context.queryClient.prefetchQuery({
-        ...context.trpc.communities.all.queryOptions(),
-        staleTime: 1000 * 60 * 2,
-      }),
-      context.queryClient.prefetchQuery({
-        ...context.trpc.communities.joined.queryOptions(),
-        staleTime: 1000 * 60 * 2,
-      }),
-    ])
+    context.queryClient.prefetchQuery({
+      ...context.trpc.communities.all.queryOptions(),
+      staleTime: 1000 * 60 * 2,
+    })
+    context.queryClient.prefetchQuery({
+      ...context.trpc.communities.joined.queryOptions(),
+      staleTime: 1000 * 60 * 2,
+    })
   },
 })
 
 function RouteComponent() {
-  const trpc = useTRPC()
-  const communities = useQuery(trpc.communities.all.queryOptions())
-  const joined = useQuery(trpc.communities.joined.queryOptions())
-  const qc = useQueryClient()
-
   const { ref, width } = useElementSize()
   const header = useElementSize()
-
-  const [api, setApi] = useState<CarouselApi>()
-  const [current, setCurrent] = useState(0)
-  const [count, setCount] = useState(0)
-  console.log("count:::", count)
-
-  useEffect(() => {
-    if (!api) {
-      return
-    }
-
-    setCount(api.scrollSnapList().length)
-    setCurrent(api.selectedScrollSnap() + 1)
-
-    api.on("select", () => {
-      setCurrent(api.selectedScrollSnap() + 1)
-    })
-  }, [api])
 
   return (
     <>
@@ -227,8 +204,8 @@ function RouteComponent() {
             <span className="text-text-sub-600">Discover</span> communities.
           </h1>
           <p className="text-pretty text-subheading-sm font-light text-text-soft-400">
-            Looking for a community? We have {communities.data?.length}+
-            communities.
+            {/* Looking for a community? We have {communities.data?.length}+
+            communities. */}
           </p>
           <Input.Root>
             <Input.Wrapper>
@@ -273,167 +250,263 @@ function RouteComponent() {
           </ul>
         </DraggableScrollContainer>
         <Grid className="mx-auto max-w-screen-2xl">
-          <Grid gap="xs" className="col-span-12 gap-8 lg:col-span-9">
-            {communities?.data?.map((c) => {
-              const images = c.images?.filter((x) => !x.logo)
-              const logo = c.images?.find((x) => x.logo)
-              return (
-                <Link
-                  onMouseOver={() =>
-                    qc.prefetchQuery(
-                      trpc.communities.detail.queryOptions({ id: c.id })
-                    )
-                  }
-                  preload="intent"
-                  to="/communities/$id"
-                  params={{
-                    id: c.id,
-                  }}
-                  key={"all-" + c.id}
-                  className="relative col-span-12 flex flex-col gap-2 lg:col-span-6"
-                >
-                  <Carousel
-                    opts={{
-                      loop: true,
-                    }}
-                    setApi={setApi}
-                    className="w-full"
-                  >
-                    <CarouselContent>
-                      {images
-                        ?.filter((x) => !x.logo)
-                        .map((image) => (
-                          <CarouselItem key={image.id}>
-                            <Image
-                              path={image.path!}
-                              lqip={{
-                                active: true,
-                                quality: 1,
-                                blur: 50,
-                              }}
-                              sizes="(min-width: 1536px) 50vw, (min-width: 1024px) 100vw"
-                              className="aspect-video w-full overflow-hidden rounded-10 object-cover"
-                              alt={`Community ${c.name} image`}
-                            />
-                          </CarouselItem>
-                        ))}
-                    </CarouselContent>
-                  </Carousel>
-
-                  <div className="relative flex items-start justify-between">
-                    <div className="absolute -top-8 left-1/2 w-fit -translate-x-1/2 rounded-full bg-white/60 px-1.5 py-1 backdrop-blur-md dark:bg-black/70">
-                      <DotStepper.Root>
-                        {images?.map((_, i) => (
-                          <DotStepper.Item
-                            key={"dot-" + i}
-                            aria-label={`Go to image ${i}`}
-                            active={i + 1 === current}
-                            onClick={() => {
-                              if (i + 1 !== current) {
-                                api?.scrollTo(i)
-                              }
-                            }}
-                          />
-                        ))}
-                      </DotStepper.Root>
-                    </div>
-                    <div className="flex flex-col gap-2">
-                      <div className="flex items-center gap-2">
-                        <Avatar.Root size="24">
-                          <Avatar.Image
-                            src={logo?.url!}
-                            alt={`Community ${c.name} image`}
-                          />
-                        </Avatar.Root>
-                        <h2 className="line-clamp-2 text-title-h6 font-light">
-                          {c.name}
-                        </h2>
-                      </div>
-                      <p className="line-clamp-3 text-pretty text-subheading-sm font-light text-text-soft-400">
-                        {c.headline}
-                      </p>
-                      <div className="flex flex-wrap items-center gap-2">
-                        {c.tags?.map((t) => (
-                          <Badge.Root
-                            variant="lighter"
-                            size="small"
-                            key={c.id + t}
-                          >
-                            {t}
-                          </Badge.Root>
-                        ))}
-                      </div>
-                    </div>
-                    <AvatarGroupCompact.Root
-                      size="24"
-                      className="bg-bg-weak-50 shadow-regular-sm"
-                    >
-                      <AvatarGroupCompact.Stack>
-                        {c.members
-                          ?.slice(0, 3)
-                          .map((m) => (
-                            <Avatar.Root>
-                              {m.avatarUrl ? (
-                                <Avatar.Image src={m.avatarUrl} />
-                              ) : (
-                                m.firstName?.[0] + m.lastName?.[0]
-                              )}
-                            </Avatar.Root>
-                          ))}
-                      </AvatarGroupCompact.Stack>
-                      {c.members?.length && c.members?.length > 3 && (
-                        <AvatarGroupCompact.Overflow>
-                          +{c.members?.length - 3}
-                        </AvatarGroupCompact.Overflow>
-                      )}
-                    </AvatarGroupCompact.Root>
-                  </div>
-                </Link>
-              )
-            })}
-          </Grid>
+          <Suspense fallback={<AllCommunitiesSkeleton />}>
+            <AllCommunities />
+          </Suspense>
           <div className="col-span-12 flex flex-col gap-4 lg:col-span-3">
             <h3 className="text-title-h6 font-light text-text-soft-400">
               Your Communities
             </h3>
-            <div className="flex flex-col gap-2">
-              {joined?.data?.map((c) => {
-                const logo = c.images?.find((x) => x.logo)
-                return (
-                  <Link
-                    onMouseOver={() =>
-                      qc.prefetchQuery(
-                        trpc.communities.detail.queryOptions({ id: c.id })
-                      )
-                    }
-                    preload="intent"
-                    to="/communities/$id"
-                    params={{
-                      id: c.id,
-                    }}
-                    key={c.id}
-                    className="flex w-full items-center gap-3 rounded-xl py-2 text-left transition-all duration-200 ease-out hover:bg-[#E9E9E9] hover:px-3 dark:hover:bg-[#191919]"
-                  >
-                    <Avatar.Root size="40">
-                      <Avatar.Image src={logo?.url!} />
-                    </Avatar.Root>
-                    <div className="min-w-0 flex-1 space-y-1">
-                      <div className="line-clamp-2 text-label-md font-light">
-                        {c.name}
-                      </div>
-                      <div className="line-clamp-1 truncate text-pretty text-subheading-xs font-light text-text-soft-400">
-                        {c.headline}
-                      </div>
-                    </div>
-
-                    <RiArrowRightLine className="size-4 fill-text-soft-400" />
-                  </Link>
-                )
-              })}
-            </div>
+            <Suspense fallback={<JoinedCommunitiesSkeleton />}>
+              <JoinedCommunities />
+            </Suspense>
           </div>
         </Grid>
       </Section>
     </>
+  )
+}
+
+function AllCommunities() {
+  const trpc = useTRPC()
+  const communities = useSuspenseQuery(trpc.communities.all.queryOptions())
+  return (
+    <Grid gap="xs" className="col-span-12 gap-8 lg:col-span-9">
+      {communities?.data?.map((c) => {
+        return <CommunityCard key={c.id} c={c} />
+      })}
+    </Grid>
+  )
+}
+
+function JoinedCommunities() {
+  const trpc = useTRPC()
+  const joined = useSuspenseQuery(trpc.communities.joined.queryOptions())
+  const qc = useQueryClient()
+  return (
+    <div className="flex flex-col gap-2">
+      {joined?.data?.map((c) => {
+        const logo = c.images?.find((x) => x.logo)
+        return (
+          <Link
+            onMouseOver={() =>
+              qc.prefetchQuery(
+                trpc.communities.detail.queryOptions({ id: c.id })
+              )
+            }
+            preload="intent"
+            to="/communities/$id"
+            params={{
+              id: c.id,
+            }}
+            key={c.id}
+            className="flex w-full items-center gap-3 rounded-xl py-2 text-left transition-all duration-200 ease-out hover:bg-[#E9E9E9] hover:px-3 dark:hover:bg-[#191919]"
+          >
+            <Avatar.Root size="40">
+              <Avatar.Image src={logo?.url!} />
+            </Avatar.Root>
+            <div className="min-w-0 flex-1 space-y-1">
+              <div className="line-clamp-2 text-label-md font-light">
+                {c.name}
+              </div>
+              <div className="line-clamp-1 truncate text-pretty text-subheading-xs font-light text-text-soft-400">
+                {c.headline}
+              </div>
+            </div>
+
+            <RiArrowRightLine className="size-4 fill-text-soft-400" />
+          </Link>
+        )
+      })}
+    </div>
+  )
+}
+
+function CommunityCard({
+  c,
+}: {
+  c: z.infer<typeof communitiesAllSchema>[number]
+}) {
+  const trpc = useTRPC()
+  const qc = useQueryClient()
+  const images = c.images?.filter((x) => !x.logo)
+  const logo = c.images?.find((x) => x.logo)
+
+  const [api, setApi] = useState<CarouselApi>()
+  const [current, setCurrent] = useState(0)
+  const [count, setCount] = useState(0)
+  console.log("count:::", count)
+
+  useEffect(() => {
+    if (!api) {
+      return
+    }
+
+    setCount(api.scrollSnapList().length)
+    setCurrent(api.selectedScrollSnap() + 1)
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1)
+    })
+  }, [api])
+
+  return (
+    <Link
+      onMouseOver={() =>
+        qc.prefetchQuery(trpc.communities.detail.queryOptions({ id: c.id }))
+      }
+      preload="intent"
+      to="/communities/$id"
+      params={{
+        id: c.id,
+      }}
+      key={"all-" + c.id}
+      className="relative col-span-12 flex flex-col gap-2 lg:col-span-6"
+    >
+      <Carousel
+        opts={{
+          loop: true,
+        }}
+        setApi={setApi}
+        className="w-full"
+      >
+        <CarouselContent>
+          {images
+            ?.filter((x) => !x.logo)
+            .map((image) => (
+              <CarouselItem key={image.id}>
+                <Image
+                  path={image.path!}
+                  lqip={{
+                    active: true,
+                    quality: 1,
+                    blur: 50,
+                  }}
+                  sizes="(min-width: 1536px) 50vw, (min-width: 1024px) 100vw"
+                  className="aspect-video w-full overflow-hidden rounded-10 object-cover"
+                  alt={`Community ${c.name} image`}
+                />
+              </CarouselItem>
+            ))}
+        </CarouselContent>
+      </Carousel>
+
+      <div className="relative flex items-start justify-between">
+        <div className="absolute -top-8 left-1/2 w-fit -translate-x-1/2 rounded-full bg-white/60 px-1.5 py-1 backdrop-blur-md dark:bg-black/70">
+          <DotStepper.Root>
+            {images?.map((_, i) => (
+              <DotStepper.Item
+                key={"dot-" + i}
+                aria-label={`Go to image ${i}`}
+                active={i + 1 === current}
+                onClick={() => {
+                  if (i + 1 !== current) {
+                    api?.scrollTo(i)
+                  }
+                }}
+              />
+            ))}
+          </DotStepper.Root>
+        </div>
+        <div className="flex flex-col gap-2">
+          <div className="flex items-center gap-2">
+            <Avatar.Root size="24">
+              <Avatar.Image
+                src={logo?.url!}
+                alt={`Community ${c.name} image`}
+              />
+            </Avatar.Root>
+            <h2 className="line-clamp-2 text-title-h6 font-light">{c.name}</h2>
+          </div>
+          <p className="line-clamp-3 text-pretty text-subheading-sm font-light text-text-soft-400">
+            {c.headline}
+          </p>
+          <div className="flex flex-wrap items-center gap-2">
+            {c.tags?.map((t) => (
+              <Badge.Root variant="lighter" size="small" key={c.id + t}>
+                {t}
+              </Badge.Root>
+            ))}
+          </div>
+        </div>
+        <AvatarGroupCompact.Root
+          size="24"
+          className="bg-bg-weak-50 shadow-regular-sm"
+        >
+          <AvatarGroupCompact.Stack>
+            {c.members
+              ?.slice(0, 3)
+              .map((m) => (
+                <Avatar.Root>
+                  {m.avatarUrl ? (
+                    <Avatar.Image src={m.avatarUrl} />
+                  ) : (
+                    m.firstName?.[0] + m.lastName?.[0]
+                  )}
+                </Avatar.Root>
+              ))}
+          </AvatarGroupCompact.Stack>
+          {c.members?.length && c.members?.length > 3 && (
+            <AvatarGroupCompact.Overflow>
+              +{c.members?.length - 3}
+            </AvatarGroupCompact.Overflow>
+          )}
+        </AvatarGroupCompact.Root>
+      </div>
+    </Link>
+  )
+}
+
+function AllCommunitiesSkeleton() {
+  return (
+    <Grid gap="xs" className="col-span-12 gap-8 lg:col-span-9">
+      <div className="relative col-span-12 flex flex-col gap-2 lg:col-span-6">
+        <div className="aspect-video w-full animate-pulse overflow-hidden rounded-10 bg-bg-weak-50 object-cover"></div>
+        <div className="flex flex-col gap-2">
+          <div className="h-4 w-1/2 animate-pulse rounded-full bg-bg-weak-50"></div>
+          <div className="h-4 w-1/3 animate-pulse rounded-full bg-bg-weak-50"></div>
+        </div>
+      </div>
+      <div className="relative col-span-12 flex flex-col gap-2 lg:col-span-6">
+        <div className="aspect-video w-full animate-pulse overflow-hidden rounded-10 bg-bg-weak-50 object-cover"></div>
+        <div className="flex flex-col gap-2">
+          <div className="h-4 w-1/2 animate-pulse rounded-full bg-bg-weak-50"></div>
+          <div className="h-4 w-1/3 animate-pulse rounded-full bg-bg-weak-50"></div>
+        </div>
+      </div>
+      <div className="relative col-span-12 flex flex-col gap-2 lg:col-span-6">
+        <div className="aspect-video w-full animate-pulse overflow-hidden rounded-10 bg-bg-weak-50 object-cover"></div>
+        <div className="flex flex-col gap-2">
+          <div className="h-4 w-1/2 animate-pulse rounded-full bg-bg-weak-50"></div>
+          <div className="h-4 w-1/3 animate-pulse rounded-full bg-bg-weak-50"></div>
+        </div>
+      </div>
+      <div className="relative col-span-12 flex flex-col gap-2 lg:col-span-6">
+        <div className="aspect-video w-full animate-pulse overflow-hidden rounded-10 bg-bg-weak-50 object-cover"></div>
+        <div className="flex flex-col gap-2">
+          <div className="h-4 w-1/2 animate-pulse rounded-full bg-bg-weak-50"></div>
+          <div className="h-4 w-1/3 animate-pulse rounded-full bg-bg-weak-50"></div>
+        </div>
+      </div>
+      <div className="relative col-span-12 flex flex-col gap-2 lg:col-span-6">
+        <div className="aspect-video w-full animate-pulse overflow-hidden rounded-10 bg-bg-weak-50 object-cover"></div>
+        <div className="flex flex-col gap-2">
+          <div className="h-4 w-1/2 animate-pulse rounded-full bg-bg-weak-50"></div>
+          <div className="h-4 w-1/3 animate-pulse rounded-full bg-bg-weak-50"></div>
+        </div>
+      </div>
+    </Grid>
+  )
+}
+
+function JoinedCommunitiesSkeleton() {
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="h-14 w-full animate-pulse rounded-xl bg-bg-weak-50"></div>
+      <div className="h-14 w-full animate-pulse rounded-xl bg-bg-weak-50"></div>
+      <div className="h-14 w-full animate-pulse rounded-xl bg-bg-weak-50"></div>
+      <div className="h-14 w-full animate-pulse rounded-xl bg-bg-weak-50"></div>
+      <div className="h-14 w-full animate-pulse rounded-xl bg-bg-weak-50"></div>
+    </div>
   )
 }
