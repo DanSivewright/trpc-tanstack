4a12279b75b55d61dd007b3b0a0ae419a6f96e29
Dan Sivewright <dan@wixels.com>
2025-05-13 20:00:35 +0200

    press and hold to delete

--- app/routes/_learner/communities/$id/index.tsx
+++ app/routes/_learner/communities/$id/index.tsx
@@ -1,26 +1,57 @@
-import { Suspense } from "react"
 import { createFileRoute } from "@tanstack/react-router"
+
+import { Section } from "@/components/section"
 
 import FeedInput from "./-components/feed-input"
 import FeedList from "./-components/feed-list"
 
 export const Route = createFileRoute("/_learner/communities/$id/")({
   loader: async ({ context, params: { id } }) => {
-    context.queryClient.prefetchQuery(
-      context.trpc.communities.detail.queryOptions({
-        id,
-      })
-    )
-    context.queryClient.prefetchQuery(
-      context.trpc.communities.feed.queryOptions({
-        communityId: id,
-      })
-    )
+    await Promise.all([
+      context.queryClient.ensureQueryData(
+        context.trpc.communities.detail.queryOptions({
+          id,
+        })
+      ),
+      context.queryClient.ensureQueryData(
+        context.trpc.communities.feed.queryOptions({
+          communityId: id,
+        })
+      ),
+    ])
     return {
       leaf: "Feed",
     }
   },
   component: RouteComponent,
+  pendingComponent: () => {
+    return (
+      <>
+        <FeedInput />
+        <div className="relative z-10 mx-auto flex w-full max-w-screen-lg flex-col gap-2 px-8 pt-6 xl:px-0">
+          <Section side="b" className="mt-6 flex flex-col gap-16">
+            {Array.from({ length: 5 }).map((_, i) => (
+              <div className="flex flex-col gap-2">
+                <div className="relative flex flex-col gap-1.5">
+                  <div className="flex items-end gap-2">
+                    <div className="flex items-center gap-2">
+                      <div className="relative size-6 animate-pulse rounded-full bg-bg-weak-50 xl:absolute xl:-left-16 xl:top-0 xl:block xl:size-12"></div>
+                      <div className="size-6 w-48 animate-pulse rounded-md bg-bg-weak-50"></div>
+                    </div>
+                  </div>
+                  <div className="size-7 w-3/4 animate-pulse rounded-md bg-bg-weak-50"></div>
+                  <div className="h-4 w-3/4 animate-pulse rounded-md bg-bg-weak-50"></div>
+                  <div className="h-4 w-full animate-pulse rounded-md bg-bg-weak-50"></div>
+                  <div className="h-4 w-1/3 animate-pulse rounded-md bg-bg-weak-50"></div>
+                  <div className="aspect-video w-full animate-pulse rounded-md bg-bg-weak-50"></div>
+                </div>
+              </div>
+            ))}
+          </Section>
+        </div>
+      </>
+    )
+  },
 })
 
 function RouteComponent() {
@@ -28,9 +59,7 @@ function RouteComponent() {
     <>
       <FeedInput />
       <div className="relative z-10 mx-auto flex w-full max-w-screen-lg flex-col gap-2 px-8 pt-6 xl:px-0">
-        <Suspense fallback={<div>Loading...</div>}>
-          <FeedList />
-        </Suspense>
+        <FeedList />
       </div>
     </>
   )
--- app/routes/_learner/communities/-components/comments.tsx
+++ app/routes/_learner/communities/-components/comments.tsx
@@ -1,11 +1,17 @@
-import { useMemo } from "react"
+import { useMemo, useState } from "react"
 import { useTRPC } from "@/integrations/trpc/react"
 import type { getCommunityCommentsSchema } from "@/integrations/trpc/routers/communities/queries"
 import { buildNestedCommentsTree } from "@/utils/build-nested-comments-tree"
+import { cn } from "@/utils/cn"
 import {
   RiAddLine,
   RiDeleteBinLine,
+  RiGlobalLine,
+  RiListRadio,
+  RiLoaderLine,
   RiMessage2Line,
+  RiMoreLine,
+  RiStarFill,
   RiThumbUpFill,
   RiThumbUpLine,
 } from "@remixicon/react"
@@ -14,14 +20,18 @@ import {
   useQueryClient,
   useSuspenseQuery,
 } from "@tanstack/react-query"
-import { formatDistance } from "date-fns"
+import { format, formatDistance } from "date-fns"
+import { motion, useAnimation } from "motion/react"
 import type { z } from "zod"
 
 import { Avatar } from "@/components/ui/avatar"
 import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
+import { Divider } from "@/components/ui/divider"
+import { Dropdown } from "@/components/ui/dropdown"
 import { toast } from "@/components/ui/toast"
 import * as AlertToast from "@/components/ui/toast-alert"
+import { Tooltip } from "@/components/ui/tooltip"
 
 import CommentWysiwyg from "./comment-wysiwyg"
 import LikesButton from "./likes-button"
@@ -148,10 +158,6 @@ const CommentsList: React.FC<CommentsListProps> = ({
     },
   })
 
-  const updateCommentMutation = useMutation({
-    ...trpc.communities.updateComment.mutationOptions(),
-  })
-
   const handleComment = async ({
     htmlString,
     parentCommentId,
@@ -201,6 +207,9 @@ const CommentsList: React.FC<CommentsListProps> = ({
     <ul className="flex w-full flex-col gap-8 pl-6">
       {comments.map((comment, ci) => {
         const isNotLast = ci !== comments.length - 1 && depth !== 0
+        const memmber = community?.data?.members?.find(
+          (m) => m?.uid === comment?.authorUid
+        )
         return (
           <li
             //
@@ -218,17 +227,65 @@ const CommentsList: React.FC<CommentsListProps> = ({
                 <div className="absolute -left-[25px] bottom-0 top-10 w-px bg-stroke-soft-200"></div>
               ) : null}
               <div className="-ml-10 flex items-center gap-2">
-                <Avatar.Root size="32">
-                  {!comment?.deletedAt && (
-                    <>
-                      {comment.author.avatarUrl ? (
-                        <Avatar.Image src={comment.author.avatarUrl} />
-                      ) : (
-                        comment?.author?.name?.[0]
+                <Tooltip.Root>
+                  <Tooltip.Trigger>
+                    <Avatar.Root size="32">
+                      {!comment?.deletedAt && (
+                        <>
+                          {comment.author.avatarUrl ? (
+                            <Avatar.Image src={comment.author.avatarUrl} />
+                          ) : (
+                            comment?.author?.name?.[0]
+                          )}
+                        </>
                       )}
-                    </>
+                    </Avatar.Root>
+                  </Tooltip.Trigger>
+                  {!comment?.deletedAt && (
+                    <Tooltip.Content
+                      size="medium"
+                      variant="light"
+                      className="max-w-[272px] p-0"
+                    >
+                      <div className="flex flex-col gap-3">
+                        <div className="flex gap-3 p-2">
+                          <Avatar.Root size="32">
+                            {comment.author.avatarUrl ? (
+                              <Avatar.Image src={comment.author.avatarUrl} />
+                            ) : (
+                              comment?.author?.name?.[0]
+                            )}
+                          </Avatar.Root>
+                          <div>
+                            <div className="text-text-strong-950">
+                              {comment?.author?.name}
+                            </div>
+
+                            <div className="mt-1 text-paragraph-xs text-text-sub-600">
+                              Member Since:{" "}
+                              {format(
+                                new Date(memmber?.joinedAt!),
+                                "d MMMM, yyy"
+                              )}
+                            </div>
+                          </div>
+                        </div>
+                        <div className="flex flex-1 gap-2 border-t border-stroke-soft-200 p-2">
+                          <Button.Root
+                            mode="lighter"
+                            className="grow"
+                            size="xxsmall"
+                          >
+                            Profile
+                          </Button.Root>
+                          <Button.Root className="grow" size="xxsmall">
+                            Chat
+                          </Button.Root>
+                        </div>
+                      </div>
+                    </Tooltip.Content>
                   )}
-                </Avatar.Root>
+                </Tooltip.Root>
                 <div className="flex flex-col">
                   <span className="text-label-sm font-medium">
                     {!comment?.deletedAt ? (
@@ -276,15 +333,13 @@ const CommentsList: React.FC<CommentsListProps> = ({
                   collectionGroup="comments"
                   collectionGroupDocId={comment.id}
                   communityId={communityId}
-                  iconLine={RiThumbUpLine}
-                  iconFill={RiThumbUpFill}
-                  mode="ghost"
+                  mode="lighter"
                   hideText
                 />
                 <Button.Root
                   size="xxsmall"
                   variant="neutral"
-                  mode="ghost"
+                  mode="lighter"
                   onClick={() =>
                     onShowReply?.(
                       comment.id === replyToCommentId ? "" : comment.id,
@@ -295,23 +350,57 @@ const CommentsList: React.FC<CommentsListProps> = ({
                   Reply
                   <Button.Icon as={RiMessage2Line} />
                 </Button.Root>
-                <Button.Root
-                  onClick={() => {
-                    updateCommentMutation.mutate({
-                      id: comment?.id,
-                      communityId,
-                      collectionGroup,
-                      collectionGroupDocId,
-                      status: "hidden",
-                      deletedAt: new Date().toISOString(),
-                    })
-                  }}
-                  size="xxsmall"
-                  variant="error"
-                  mode="ghost"
-                >
-                  <Button.Icon as={RiDeleteBinLine} />
-                </Button.Root>
+                <Dropdown.Root>
+                  <Dropdown.Trigger asChild>
+                    <Button.Root
+                      size="xxsmall"
+                      variant="neutral"
+                      mode="lighter"
+                      // onClick={() => {
+                      //   updateCommentMutation.mutate({
+                      //     id: comment?.id,
+                      //     communityId,
+                      //     collectionGroup,
+                      //     collectionGroupDocId,
+                      //     status: "hidden",
+                      //     deletedAt: new Date().toISOString(),
+                      //   })
+                      // }}
+                      // size="xxsmall"
+                      // variant="error"
+                      // mode="ghost"
+                    >
+                      <Button.Icon as={RiMoreLine} />
+                    </Button.Root>
+                  </Dropdown.Trigger>
+                  <Dropdown.Content
+                    align="end"
+                    className="bg-white/80 backdrop-blur"
+                  >
+                    <Dropdown.Group>
+                      <Dropdown.Item>
+                        <Dropdown.ItemIcon
+                          className="fill-warning-base"
+                          as={RiStarFill}
+                        />
+                        Feature Comment
+                      </Dropdown.Item>
+                      <Dropdown.Item>
+                        <Dropdown.ItemIcon as={RiListRadio} />
+                        Add To Feed
+                      </Dropdown.Item>
+                    </Dropdown.Group>
+                    <Divider.Root variant="line-spacing" />
+                    <Dropdown.Group>
+                      <DeleteComment
+                        communityId={communityId}
+                        collectionGroup={collectionGroup}
+                        collectionGroupDocId={collectionGroupDocId}
+                        comment={comment}
+                      />
+                    </Dropdown.Group>
+                  </Dropdown.Content>
+                </Dropdown.Root>
               </footer>
               {replyToCommentId === comment.id && (
                 <CommentWysiwyg
@@ -340,5 +429,103 @@ const CommentsList: React.FC<CommentsListProps> = ({
       })}
       {children}
     </ul>
+  )
+}
+
+function DeleteComment({
+  communityId,
+  collectionGroup,
+  collectionGroupDocId,
+  comment,
+}: Omit<CommentsListProps, "comments" | "depth"> & {
+  comment: ReturnType<typeof buildNestedCommentsTree>[number]
+}) {
+  const trpc = useTRPC()
+  const HOLD_DURATION = 1000
+  const [_, setIsHolding] = useState(false)
+  const controls = useAnimation()
+  const queryClient = useQueryClient()
+
+  const updateCommentMutation = useMutation({
+    ...trpc.communities.updateComment.mutationOptions(),
+    onMutate: async (newComment) => {
+      await queryClient.cancelQueries({
+        queryKey: trpc.communities.comments.queryOptions({
+          collectionGroup,
+          collectionGroupDocId,
+          communityId,
+        }).queryKey,
+      })
+      return undefined
+    },
+  })
+
+  async function handleHoldStart(
+    e: React.MouseEvent<HTMLDivElement> | React.TouchEvent<HTMLDivElement>
+  ) {
+    e.preventDefault()
+    e.stopPropagation()
+    setIsHolding(true)
+    controls.set({ width: "0%" })
+    await controls.start({
+      width: "100%",
+      transition: {
+        duration: HOLD_DURATION / 1000,
+        ease: "linear",
+      },
+    })
+  }
+
+  function handleHoldEnd(
+    e: React.MouseEvent<HTMLDivElement> | React.TouchEvent<HTMLDivElement>
+  ) {
+    e.preventDefault()
+    e.stopPropagation()
+    setIsHolding(false)
+    controls.stop()
+    controls.start({
+      width: "0%",
+      transition: { duration: 0.1 },
+    })
+    updateCommentMutation.mutate({
+      id: comment.id,
+      communityId,
+      collectionGroup,
+      collectionGroupDocId,
+      status: "hidden",
+      deletedAt: new Date().toISOString(),
+    })
+  }
+  return (
+    <Dropdown.Item
+      disabled={updateCommentMutation.isPending}
+      onClick={(e) => {
+        e.preventDefault()
+        e.stopPropagation()
+      }}
+      onMouseDown={handleHoldStart}
+      onMouseUp={handleHoldEnd}
+      onMouseLeave={handleHoldEnd}
+      onTouchStart={handleHoldStart}
+      onTouchEnd={handleHoldEnd}
+      onTouchCancel={handleHoldEnd}
+      className="relative overflow-hidden bg-error-lighter text-error-dark data-[highlighted]:bg-error-lighter"
+    >
+      <motion.div
+        initial={{ width: "0%" }}
+        animate={controls}
+        className={cn("absolute left-0 top-0 z-0 h-full", "bg-error-light")}
+      />
+      <Dropdown.ItemIcon
+        className={cn(
+          "relative z-10",
+          updateCommentMutation.isPending && "animate-spin"
+        )}
+        as={updateCommentMutation.isPending ? RiLoaderLine : RiDeleteBinLine}
+      />
+      <span className="relative z-10">
+        {updateCommentMutation.isPending ? "Deleting..." : "Delete Comment"}
+      </span>
+    </Dropdown.Item>
   )
 }
